f0c9dcf8c205cc72a57feb86f30e889dd1d09606
Namira <namirak96@.com>
2026-02-12 16:29:24 +0530

    changes

--- email_verifier/urls.py
+++ email_verifier/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("verify/", views.verify_email_file, name="verify_email_file"),
+    path("verify/", views.verify_email_file, name="verify"),
     path("download/csv/", views.download_verified_emails, name="download_csv"),
     path("download/xlsx/", views.download_verified_emails_xlsx, name="download_xlsx"),
 ]
