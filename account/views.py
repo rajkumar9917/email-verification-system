7e0c7463c12fe4a976177edaaf4007d0015ef80f
Jeetendra Dhangar <jeetendradhangar6@gmail.com>
2026-02-14 13:32:24 +0530

    UI Uptaded

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register_view(request):
+    return render(request, 'account/register.html')
+
+def login_view(request):
+    return render(request, 'account/login.html')
--- email_system/urls.py
+++ email_system/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from email_system import views
 
 from email_system import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('', include('email_verifier.urls')),
     path('', include('bulk_sender.urls')),
+    path('', include('account.urls')),
 ]
 
 if settings.DEBUG:
